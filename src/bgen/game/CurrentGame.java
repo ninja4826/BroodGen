1ad27fe141fc2ed898ace205e55c3babc9edc721
ninja4826 <hueske.russ690@gmail.com>
2014-11-04 09:02:10 -0600

    stuff

--- src/bgen/game/CurrentGame.java
+++ src/bgen/game/CurrentGame.java
@@ -9,14 +9,14 @@ public class CurrentGame {
 	public static Struct[] currentStructs;
 	public static Upgrade[] currentUpgrades = new Upgrade[]{};
 	public static GameEngine game;
-	
+
 	public static void newGame(String race) {
 		CurrentGame.game = new GameEngine(race);
-		
+
 		CurrentGame.resources[0] = CurrentGame.game.startRecs[1] - CurrentGame.game.startRecs[0];
 		CurrentGame.currentUnits = CurrentGame.game.startUnits;
 		CurrentGame.currentStructs = new Struct[]{CurrentGame.game.startBase};
 	}
-	
+
 
 }
